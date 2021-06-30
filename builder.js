eb03f506657f0ceccac290e9e4173699e3b9fbe1
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-06-30 12:47:51 +0200

    🖋 Update

--- builder.js
+++ builder.js
@@ -1,5 +1,6 @@
 const animeback_submit = require('@taistudio/animeback-submit');
 const fs = require('fs');
+fs.writeFileSync('index.json', JSON.stringify(animeback_submit));
 
 const cheerio = require('cheerio');
 const $ = cheerio.load('<meta http-equiv="Content-Type" content="application/json; charset=UTF-8" />');
